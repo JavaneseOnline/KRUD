5b08475c0c15d36858b7c1a0c6a14063a69658cc
Miha_x64 <mihlo1996@rambler.ru>
2017-11-01 23:35:32 +0300

    added Module kdoc

--- krud-core/src/main/kotlin/online/javanese/krud/Module.kt
+++ krud-core/src/main/kotlin/online/javanese/krud/Module.kt
@@ -5,10 +5,20 @@ import org.jetbrains.ktor.application.ApplicationCall
 import org.jetbrains.ktor.http.HttpMethod
 import org.jetbrains.ktor.util.ValuesMap
 
+/**
+ * Describes a part of admin panel.
+ * @see online.javanese.krud.crud.Crud
+ */
 interface Module {
 
+    /**
+     * This name is going to be shown in the sidebar.
+     */
     val name: String
 
+    /**
+     * Handle a request. Finally, must call `call.respond...`.
+     */
     suspend fun request(
             call: ApplicationCall,
             routePrefix: String,
