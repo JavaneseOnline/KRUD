8720c5e785ab9b41328b818b8211647756bf5dad
Miha_x64 <mihlo1996@rambler.ru>
2017-11-03 22:49:50 +0300

    rm dynamically added CSS from template

--- krud-core/src/main/kotlin/online/javanese/krud/template/MaterialTemplate.kt
+++ krud-core/src/main/kotlin/online/javanese/krud/template/MaterialTemplate.kt
@@ -33,7 +33,6 @@ class MaterialTemplate(
 
                 styleLink("https://fonts.googleapis.com/css?family=PT+Sans:400,700&subset=latin,cyrillic")
                 styleLink("//fonts.googleapis.com/icon?family=Material+Icons")
-                // <link rel="stylesheet" th:href="@{/sandbox/codemirror_ambiance.min.css}" />
                 styleLink("https://cdnjs.cloudflare.com/ajax/libs/dialog-polyfill/0.4.3/dialog-polyfill.min.css")
                 styleLink("$staticPath/admin-material.min.css")
                 styleLink("$staticPath/dashboard.css")
