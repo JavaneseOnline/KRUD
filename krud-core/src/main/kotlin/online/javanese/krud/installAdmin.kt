09bf0fd77126754232e91635050ed86d54dde07d
Miha_x64 <mihlo1996@rambler.ru>
2017-11-26 15:33:04 +0300

    added Route.installAdmin(AdminPanel) function

---
+++ krud-core/src/main/kotlin/online/javanese/krud/installAdmin.kt
@@ -0,0 +1,49 @@
+package online.javanese.krud
+
+import io.ktor.application.call
+import io.ktor.http.HttpMethod
+import io.ktor.request.receiveParameters
+import io.ktor.routing.Route
+import io.ktor.routing.get
+import io.ktor.routing.post
+import io.ktor.util.ValuesMap
+import io.ktor.websocket.webSocket
+
+fun Route.installAdmin(admin: AdminPanel) {
+    get("") {
+        admin.dashboard(call)
+    }
+
+    get("{module}/{tail...}") {
+        admin.http(
+                call,
+                HttpMethod.Get,
+                call.parameters["module"]!!,
+                call.parameters.getAll("tail")!!,
+                call.parameters,
+                ValuesMap.Empty
+        )
+    }
+
+    post("{module}/{tail...}") {
+        admin.http(
+                call,
+                HttpMethod.Post,
+                call.parameters["module"]!!,
+                call.parameters.getAll("tail")!!,
+                call.parameters,
+                call.receiveParameters()
+        )
+    }
+
+    webSocket(path = "{module}/{tail...}") {
+        admin.webSocket(
+                call,
+                call.parameters["module"]!!,
+                call.parameters.getAll("tail")!!,
+                call.parameters,
+                incoming,
+                outgoing
+        )
+    }
+}
