a7905da18f193011f9a80b0908476259dd91860d
Miha_x64 <mihlo1996@rambler.ru>
2017-12-12 16:14:53 +0300

    rm obsolete comment about static resources

--- sample/src/main/kotlin/KrudTestServer.kt
+++ sample/src/main/kotlin/KrudTestServer.kt
@@ -29,7 +29,6 @@ object KrudTestServer {
 
     /**
      * Starts test server.
-     * Required static dir address to be specified through CLI arguments.
      * After start, you should be able to see a working admin-panel
      * at http://localhost:8081/admin/.
      */
