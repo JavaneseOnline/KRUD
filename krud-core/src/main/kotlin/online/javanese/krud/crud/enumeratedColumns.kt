5f200f696f627d0fdf0b7ff38e682997b3f4a01b
Miha_x64 <mihlo1996@rambler.ru>
2017-12-21 15:31:37 +0300

    reworked EnumeratedCol so it can could work with foreign keys

--- krud-core/src/main/kotlin/online/javanese/krud/crud/enumeratedColumns.kt
+++ krud-core/src/main/kotlin/online/javanese/krud/crud/enumeratedColumns.kt
@@ -8,10 +8,11 @@ import kotlin.reflect.KProperty1
 /**
  * Describes values of an enumeration.
  */
-interface EnumeratedColAdapter<T> {
+interface EnumeratedColAdapter<T, TID> {
     fun elements(): Iterable<T>
-    fun nameOf(t: T): String
-    fun titleOf(t: T): String
+    fun idOf(t: T): TID
+    fun element(id: TID): T
+    fun titleOf(element: T): String
 }
 
 /**
@@ -19,50 +20,53 @@ interface EnumeratedColAdapter<T> {
  */
 class EnumColAdapter<E : Enum<E>>(
         klass: Class<E>,
-        private val getName: (E) -> String = Enum<E>::name,
-        private val getTitle: (E) -> String = getName
-) : EnumeratedColAdapter<E> {
+        private val getTitle: (E) -> String = Enum<E>::name
+) : EnumeratedColAdapter<E, E> {
     private val el = Collections.unmodifiableList(klass.enumConstants!!.toList())
     override fun elements(): Iterable<E> = el
-    override fun nameOf(t: E): String = t.name
-    override fun titleOf(t: E): String = getTitle(t)
+    override fun idOf(t: E): E = t // enum is ID of itself
+    override fun element(id: E): E = id // same
+    override fun titleOf(element: E): String = getTitle(element)
 }
 
 /**
  * Shortcut factory for [EnumColAdapter]
  */
 inline fun <reified E : Enum<E>> EnumColAdapter(
-        noinline getName: (E) -> String = Enum<E>::name,
-        noinline getTitle: (E) -> String = getName
-) = EnumColAdapter(E::class.java, getName, getTitle)
+        noinline getTitle: (E) -> String = Enum<E>::name
+) = EnumColAdapter(E::class.java, getTitle)
 
 
 /**
  * Value of such column must be chosen from a provided list.
+ * @param T related entity type
+ * @param TID T's primary key / ID
  */
-class EnumeratedCol<OWNR : Any, T>(
-        private val getValue: (OWNR) -> T,
-        private val adapter: EnumeratedColAdapter<T>,
+class EnumeratedCol<OWNR : Any, T, TID : Any>(
+        private val getId: (OWNR) -> TID,
+        private val adapter: EnumeratedColAdapter<T, TID>,
         override val name: String,
         private val title: String = name.capitalize(),
+        private val idToString: (TID) -> String = Any::toString,
         private val controlFactory: (name: String, title: String, names: List<String>, titles: List<String>) -> Control = ComboBox
 ) : Column<OWNR> {
 
     constructor(
-            property: KProperty1<OWNR, T>,
-            adapter: EnumeratedColAdapter<T>,
+            property: KProperty1<OWNR, TID>,
+            adapter: EnumeratedColAdapter<T, TID>,
             title: String = property.name.capitalize(),
+            idToString: (TID) -> String = Any::toString,
             controlFactory: (name: String, title: String, names: List<String>, titles: List<String>) -> Control = ComboBox
     ) : this(
-            property.getter, adapter, property.name, title, controlFactory
+            property.getter, adapter, property.name, title, idToString, controlFactory
     )
 
-    override fun getValue(owner: OWNR): String = adapter.nameOf(getValue.invoke(owner))
+    override fun getValue(owner: OWNR): String = idToString(getId(owner))
     override val createControl: Control get() = cc()
     override val editControl: Control get() = cc()
 
     private fun cc(): Control {
         val els = adapter.elements()
-        return controlFactory(name, title, els.map(adapter::nameOf), els.map(adapter::titleOf))
+        return controlFactory(name, title, els.map { idToString(adapter.idOf(it)) }, els.map(adapter::titleOf))
     }
 }
